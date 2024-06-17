f8f1d4e077cdfe1af4dcce54d01565cb8c80820c
ecieren <emmanuel.cieren@mews-labs.com>
2024-06-17 17:38:54 +0200

    wip

--- test/test_shape.py
+++ test/test_shape.py
@@ -3,75 +3,71 @@ import numpy as np
 from thermohl import solver
 
 
+def _solvers():
+    return [solver._factory(dic=None, heateq='1t', models=m) for m in ['cigre', 'ieee']]
+
+
 def test_0():
-    # todo: loop on heateq and models ..
+    for s in _solvers():
+        t = s.steady_temperature()
+        i = s.steady_intensity(T=t['T'].values)
 
-    s = solver._factory(dic=None, heateq='1t', models='ieee')
-    t = s.steady_temperature()
-    i = s.steady_intensity(T=t['T'].values)
-
-    assert len(t) == 1
-    assert len(i) == 1
+        assert len(t) == 1
+        assert len(i) == 1
 
 
 def test_1():
-    # todo: loop on heateq and models ..
-
     n = 61
-    s = solver._factory(dic=None, heateq='1t', models='ieee')
-    s.args.Ta = np.linspace(-10, +50, n)
-    s.update()
-    t = s.steady_temperature()
-    i = s.steady_intensity(T=t['T'].values)
+    for s in _solvers():
+        s.args.Ta = np.linspace(-10, +50, n)
+        s.update()
+        t = s.steady_temperature()
+        i = s.steady_intensity(T=t['T'].values)
 
-    assert len(t) == n
-    assert len(i) == n
+        assert len(t) == n
+        assert len(i) == n
 
 
 def test_2():
-    # todo: loop on heateq and models ..
-
     n = 61
-    s = solver._factory(dic=None, heateq='1t', models='ieee')
-    s.args.Ta = np.linspace(-10, +50, n)
-    s.args.I = np.array([199.])
-    s.update()
-    t = s.steady_temperature()
-    i = s.steady_intensity(T=t['T'].values)
+    for s in _solvers():
+        s.args.Ta = np.linspace(-10, +50, n)
+        s.args.I = np.array([199.])
+        s.update()
+        t = s.steady_temperature()
+        i = s.steady_intensity(T=t['T'].values)
 
-    assert len(t) == n
-    assert len(i) == n
+        assert len(t) == n
+        assert len(i) == n
 
 
 def test_3():
-    # todo: loop on heateq and models ..
+    for s in _solvers():
+        t = np.linspace(0, 3600, 361)
+        I = 199 * np.ones_like(t)
 
-    s = solver._factory(dic=None, heateq='1t', models='ieee')
-    t = np.linspace(0, 3600, 361)
-    I = 199 * np.ones_like(t)
+        ds = s.steady_temperature()
+        r = s.transient_temperature(t, T0=ds['T'].values[0], transit=I)
+        r = s.transient_temperature(t, T0=ds['T'].values, transit=I, return_power=True)
 
-    ds = s.steady_temperature()
-    r = s.transient_temperature(t, T0=ds['T'].values[0], transit=I)
-    r = s.transient_temperature(t, T0=ds['T'].values, transit=I, return_power=True)
-
-    assert len(r['time']) == len(t)
-    assert r['T'].shape == (len(t),)
+        assert len(r['time']) == len(t)
+        assert r['T'].shape == (len(t),)
 
 
 def test_4():
     # todo: loop on heateq and models ..
 
     n = 7
-    s = solver._factory(dic=None, heateq='1t', models='ieee')
-    s.args.Ta = np.linspace(-10, +50, n)
-    s.update()
+    for s in _solvers():
+        s.args.Ta = np.linspace(-10, +50, n)
+        s.update()
 
-    t = np.linspace(0, 3600, 361)
-    I = 199 * np.ones_like(t)
+        t = np.linspace(0, 3600, 361)
+        I = 199 * np.ones_like(t)
 
-    ds = s.steady_temperature()
-    r = s.transient_temperature(t, T0=ds['T'].values, transit=I)
-    r = s.transient_temperature(t, T0=ds['T'].values, transit=I, return_power=True)
+        ds = s.steady_temperature()
+        r = s.transient_temperature(t, T0=ds['T'].values, transit=I)
+        r = s.transient_temperature(t, T0=ds['T'].values, transit=I, return_power=True)
 
-    assert len(r['time']) == len(t)
-    assert r['T'].shape == (len(t), s.args.max_len())
+        assert len(r['time']) == len(t)
+        assert r['T'].shape == (len(t), s.args.max_len())
