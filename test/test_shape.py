0c388740fc763499adef8b4496ca1849bb1576e9
ecieren <emmanuel.cieren@mews-labs.com>
2024-06-18 15:54:11 +0200

    wip
    Signed-off-by: SAINTIER Francois <francois.saintier@rte-france.com>

--- test/test_shape.py
+++ test/test_shape.py
@@ -10,7 +10,7 @@ def _solvers():
 def test_0():
     for s in _solvers():
         t = s.steady_temperature()
-        i = s.steady_intensity(T=t['T'].values)
+        i = s.steady_intensity(T=t['t'].values)
 
         assert len(t) == 1
         assert len(i) == 1
@@ -22,7 +22,7 @@ def test_1():
         s.args.Ta = np.linspace(-10, +50, n)
         s.update()
         t = s.steady_temperature()
-        i = s.steady_intensity(T=t['T'].values)
+        i = s.steady_intensity(T=t['t'].values)
 
         assert len(t) == n
         assert len(i) == n
@@ -35,7 +35,7 @@ def test_2():
         s.args.I = np.array([199.])
         s.update()
         t = s.steady_temperature()
-        i = s.steady_intensity(T=t['T'].values)
+        i = s.steady_intensity(T=t['t'].values)
 
         assert len(t) == n
         assert len(i) == n
@@ -47,8 +47,8 @@ def test_3():
         I = 199 * np.ones_like(t)
 
         ds = s.steady_temperature()
-        r = s.transient_temperature(t, T0=ds['T'].values[0], transit=I)
-        r = s.transient_temperature(t, T0=ds['T'].values, transit=I, return_power=True)
+        r = s.transient_temperature(t, T0=ds['t'].values[0], transit=I)
+        r = s.transient_temperature(t, T0=ds['t'].values, transit=I, return_power=True)
 
         assert len(r['time']) == len(t)
         assert r['T'].shape == (len(t),)
@@ -66,8 +66,8 @@ def test_4():
         I = 199 * np.ones_like(t)
 
         ds = s.steady_temperature()
-        r = s.transient_temperature(t, T0=ds['T'].values, transit=I)
-        r = s.transient_temperature(t, T0=ds['T'].values, transit=I, return_power=True)
+        r = s.transient_temperature(t, T0=ds['t'].values, transit=I)
+        r = s.transient_temperature(t, T0=ds['t'].values, transit=I, return_power=True)
 
         assert len(r['time']) == len(t)
         assert r['T'].shape == (len(t), s.args.max_len())
