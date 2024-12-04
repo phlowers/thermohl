80f347934966b668b186794946ad75a0c3db31f7
Jeremy VERNAY <jeremy.vernay@rte-france.com>
2024-12-04 17:19:18 +0100

    DLR-1114 : typage du code pour passage en open source

--- src/thermohl/utils.py
+++ src/thermohl/utils.py
@@ -1,3 +1,4 @@
+# mypy: ignore-errors
 """Misc. utility code for thermohl project."""
 import os
 
@@ -6,7 +7,9 @@ import pandas as pd
 import yaml
 
 
-def _dict_completion(dat: dict, filename: str, check: bool = True, warning: bool = False) -> dict:
+def _dict_completion(
+    dat: dict, filename: str, check: bool = True, warning: bool = False
+) -> dict:
     """Complete input dict with values from file.
 
     Read dict stored in filename (yaml format) and for each key in it, add it
@@ -26,15 +29,22 @@ def _dict_completion(dat: dict, filename: str, check: bool = True, warning: bool
 
     """
     fil = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
-    dfl = yaml.safe_load(open(fil, 'r'))
+    dfl = yaml.safe_load(open(fil, "r"))
     for k in dfl.keys():
         if k not in dat.keys() or dat[k] is None:
             dat[k] = dfl[k]
             if warning:
-                print('Added key %s from default parameters' % (k,))
-        elif not isinstance(dat[k], int) and not isinstance(dat[k], float) and \
-                not isinstance(dat[k], np.ndarray) and check:
-            raise TypeError('element in input dict (key [%s]) must be int, float or numpy.ndarray' % (k,))
+                print("Added key %s from default parameters" % (k,))
+        elif (
+            not isinstance(dat[k], int)
+            and not isinstance(dat[k], float)
+            and not isinstance(dat[k], np.ndarray)
+            and check
+        ):
+            raise TypeError(
+                "element in input dict (key [%s]) must be int, float or numpy.ndarray"
+                % (k,)
+            )
     return dat
 
 
@@ -54,7 +64,9 @@ def add_default_parameters(dat: dict, warning: bool = False) -> dict:
         Completed input dict if some parameters were missing.
 
     """
-    fil = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'default_values.yaml')
+    fil = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "default_values.yaml"
+    )
     return _dict_completion(dat, fil, warning=warning)
 
 
@@ -74,7 +86,9 @@ def add_default_uncertainties(dat: dict, warning: bool = False) -> dict:
         Completed input dict if some parameters were missing.
 
     """
-    fil = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'default_uncertainties.yaml')
+    fil = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "default_uncertainties.yaml"
+    )
     return _dict_completion(dat, fil, check=False, warning=warning)
 
 
@@ -92,7 +106,7 @@ def df2dct(df: pd.DataFrame) -> dict:
     dict
         DESCRIPTION.
     """
-    q = df.to_dict(orient='list')
+    q = df.to_dict(orient="list")
     for k in q.keys():
         if len(q[k]) > 1:
             q[k] = np.array(q[k])
