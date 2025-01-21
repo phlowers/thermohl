fix(DLR-1159): Add units tests (#11)
* fix(DLR-1159): Add units tests

- Reformatting code with Black
- Adding unit tests
- Adding docstring
- Renaming variables

Signed-off-by: SaintierFr <99645240+saintierfr@users.noreply.github.com>>
Signed-off-by: SaintierFr <99645240+SaintierFr@users.noreply.github.com>

examples/plot_circular_statistic.py
@@ -2,7 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import circmean, circstd, circvar, vonmises_line
 
-from thermohl.distributions import _vonmises_kappa, _vonmises_circ_var, truncnorm, wrapnorm, WrappedNormal, vonmises
+from thermohl.distributions import (
+    _vonmises_kappa,
+    _vonmises_circ_var,
+    truncnorm,
+    wrapnorm,
+    WrappedNormal,
+    vonmises,
+)
 
 _twopi = 2 * np.pi
 
@@ -39,13 +46,13 @@ def circular_moments_wrapnorm():
     # as desired std is low; then for larger sg the mean is noisy and the std
     # and the variance tend to pi
     fig, ax = plt.subplots(nrows=3, ncols=1)
-    ax[0].plot(sg, cavg, label='circ. mean')
-    ax[0].plot(sg, mu, '--', c='gray', label='input mean')
-    ax[1].plot(sg, cvar, label='circ. variance')
-    ax[1].plot(sg, 1 - np.exp(-0.5 * sg**2), label='analytic')
-    ax[2].plot(sg, cstd, label='circ. std')
-    ax[2].plot(sg, sg, '--', c='gray')
-    ax[2].axhline(y=np.pi, xmin=sg[0], xmax=sg[-1], c='gray', ls='--')
+    ax[0].plot(sg, cavg, label="circ. mean")
+    ax[0].plot(sg, mu, "--", c="gray", label="input mean")
+    ax[1].plot(sg, cvar, label="circ. variance")
+    ax[1].plot(sg, 1 - np.exp(-0.5 * sg**2), label="analytic")
+    ax[2].plot(sg, cstd, label="circ. std")
+    ax[2].plot(sg, sg, "--", c="gray")
+    ax[2].axhline(y=np.pi, xmin=sg[0], xmax=sg[-1], c="gray", ls="--")
     for i in range(3):
         ax[i].grid(True)
         ax[i].legend()
@@ -96,35 +103,35 @@ def circular_moments_vonmises():
 
     plt.figure()
     plt.title("Von mises parameter and input std")
-    plt.plot(sg, kp, label='computed inverse')
-    plt.plot(sg, 1 / sg**2, '--', c='gray', label='classical approx')
-    plt.xlabel('$\sigma$')
-    plt.ylabel('$\kappa$')
+    plt.plot(sg, kp, label="computed inverse")
+    plt.plot(sg, 1 / sg**2, "--", c="gray", label="classical approx")
+    plt.xlabel("$\sigma$")
+    plt.ylabel("$\kappa$")
     plt.grid(True)
     plt.legend()
 
     fig, ax = plt.subplots(nrows=3, ncols=1)
     # computed circular mean matches distribution's mean() method;
-    ax[0].plot(sg, cavg, label='circ. mean')
-    ax[0].plot(sg, savg, label='dist. mean')
-    ax[0].plot(sg, mu, '--', c='gray', label='input mean')
+    ax[0].plot(sg, cavg, label="circ. mean")
+    ax[0].plot(sg, savg, label="dist. mean")
+    ax[0].plot(sg, mu, "--", c="gray", label="input mean")
 
     # computed circular variance does not match distribution's var() method;
     # analytic formula from wikipedia matches computed circular variance;
     # actually, d.var() = d.std()**2
-    ax[1].plot(sg, cvar, label='circ. variance')
-    ax[1].plot(sg, svar, label='dist. variance')
-    ax[1].plot(sg, _vonmises_circ_var(kp), label='analytic')
+    ax[1].plot(sg, cvar, label="circ. variance")
+    ax[1].plot(sg, svar, label="dist. variance")
+    ax[1].plot(sg, _vonmises_circ_var(kp), label="analytic")
     # ax[1].plot(sg, 0.5 / kp, ls='--', c='gray', label='1/$2\kappa$')
     # ax[1].plot(sg, 1.0 / kp, ls='--', c='gray', label='1/$2\kappa$')
     ax[1].set_ylim([0, 1.1])
 
     # computed circular std matches distribution's std() method;
-    ax[2].plot(sg, cstd, label='circ. std')
-    ax[2].plot(sg, sstd, label='dist. std')
-    ax[2].plot(sg, np.sqrt(-2 * np.log(1 - _vonmises_circ_var(kp))), label='examples')
-    ax[2].axhline(y=np.pi, xmin=sg[0], xmax=sg[-1], c='gray', ls='--')
-    ax[2].axhline(y=_twopi / np.sqrt(12), xmin=sg[0], xmax=sg[-1], c='gray', ls='--')
+    ax[2].plot(sg, cstd, label="circ. std")
+    ax[2].plot(sg, sstd, label="dist. std")
+    ax[2].plot(sg, np.sqrt(-2 * np.log(1 - _vonmises_circ_var(kp))), label="examples")
+    ax[2].axhline(y=np.pi, xmin=sg[0], xmax=sg[-1], c="gray", ls="--")
+    ax[2].axhline(y=_twopi / np.sqrt(12), xmin=sg[0], xmax=sg[-1], c="gray", ls="--")
     # ax[2].plot(sg, 1 / np.sqrt(kp), ls='--', c='gray', label='$1/\sqrt{\kappa}$')
 
     for i in range(3):
@@ -134,8 +141,15 @@ def circular_moments_vonmises():
     return
 
 
-def _common_distrib(dist, ns: int, ci=0.95, circular: bool = False, plot: bool = False, normalize: float = 1.,
-                    bounds=None):
+def _common_distrib(
+    dist,
+    ns: int,
+    ci=0.95,
+    circular: bool = False,
+    plot: bool = False,
+    normalize: float = 1.0,
+    bounds=None,
+):
     # num bins
     nb = max(20, (ns + 1) // 100)
     # sample
@@ -157,45 +171,48 @@ def _common_distrib(dist, ns: int, ci=0.95, circular: bool = False, plot: bool =
 
     if plot:
         fig, ax = plt.subplots()
-        ax.hist(s / normalize, bins=nb, density=True, fc='C1')
-        ax.plot(x / normalize, y * normalize, '-', c='C0', label='pdf')
-        ax.axvline(dist.mean() / normalize, ls='--', c='C0', label='mean')
+        ax.hist(s / normalize, bins=nb, density=True, fc="C1")
+        ax.plot(x / normalize, y * normalize, "-", c="C0", label="pdf")
+        ax.axvline(dist.mean() / normalize, ls="--", c="C0", label="mean")
         ax.grid(True)
         ax.set_xlim([a / normalize, b / normalize])
-        if normalize == 1.:
-            ax.set_xlabel('$x$')
+        if normalize == 1.0:
+            ax.set_xlabel("$x$")
         else:
-            ax.set_xlabel(f'$x$ (normalized by {normalize:.3f})')
+            ax.set_xlabel(f"$x$ (normalized by {normalize:.3f})")
         ax.grid(True)
         ax.legend()
 
-    print(f"distrib: "
-          f"min={a:+.3E},"
-          f" max={b:+.3E},"
-          f" avg={dist.mean():+3.2E}, "
-          f"std={dist.std():+3.2E}, "
-          f"ci={0.5 * (dist.ppf(qmax) - dist.ppf(qmin)) / dist.std():+3.2E}"
-          f"({100 * ci:.0f}%, std-norm.)"
-          )
+    print(
+        f"distrib: "
+        f"min={a:+.3E},"
+        f" max={b:+.3E},"
+        f" avg={dist.mean():+3.2E}, "
+        f"std={dist.std():+3.2E}, "
+        f"ci={0.5 * (dist.ppf(qmax) - dist.ppf(qmin)) / dist.std():+3.2E}"
+        f"({100 * ci:.0f}%, std-norm.)"
+    )
 
     if not circular:
-        print(f"sampled: "
-              f"min={np.min(s):+.3E}, "
-              f"max={np.max(s):+.3E}, "
-              f"avg={np.mean(s):+.3E}, "
-              f"std={np.std(s):+.3E}, "
-              f"ci={0.5 * (np.quantile(s, qmax) - np.quantile(s, qmin)) / dist.std():+.3E} "
-              f"({100 * ci:.0f}%, std-norm.)"
-              )
+        print(
+            f"sampled: "
+            f"min={np.min(s):+.3E}, "
+            f"max={np.max(s):+.3E}, "
+            f"avg={np.mean(s):+.3E}, "
+            f"std={np.std(s):+.3E}, "
+            f"ci={0.5 * (np.quantile(s, qmax) - np.quantile(s, qmin)) / dist.std():+.3E} "
+            f"({100 * ci:.0f}%, std-norm.)"
+        )
     else:
-        print(f"sampled: "
-              f"min={np.min(s):+.3E}, "
-              f"max={np.max(s):+.3E}, "
-              f"avg={circmean(s, high=b, low=a):+.3E}, "
-              f"std={circstd(s, high=b, low=a):+.3E}, "
-              f"ci={0.5 * (np.quantile(s, qmax) - np.quantile(s, qmin)) / dist.std():+.3E} "
-              f"({100 * ci:.0f}%, std-norm.)"
-              )
+        print(
+            f"sampled: "
+            f"min={np.min(s):+.3E}, "
+            f"max={np.max(s):+.3E}, "
+            f"avg={circmean(s, high=b, low=a):+.3E}, "
+            f"std={circstd(s, high=b, low=a):+.3E}, "
+            f"ci={0.5 * (np.quantile(s, qmax) - np.quantile(s, qmin)) / dist.std():+.3E} "
+            f"({100 * ci:.0f}%, std-norm.)"
+        )
 
     return
 
@@ -207,7 +224,7 @@ def example_truncnorm(a, b, mu, sigma, n=9999, ci=0.95, plot=False):
 
 
 def example_wrapnorm(mu, sigma, n=999, ci=0.95, plot=False):
-    a = 0.
+    a = 0.0
     b = _twopi
     d = wrapnorm(mu, sigma)
     _common_distrib(d, n, ci=ci, plot=plot, normalize=np.pi, bounds=(a, b))
@@ -222,15 +239,15 @@ def example_vonmises(mu, sigma, n=999, ci=0.95, plot=False):
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import matplotlib
 
-    matplotlib.use('TkAgg')
-    plt.close('all')
+    matplotlib.use("TkAgg")
+    plt.close("all")
 
     circular_moments_wrapnorm()
     circular_moments_vonmises()
 
-    example_truncnorm(-1., 1., 0.318, 0.2, plot=True)
-    example_wrapnorm(1., 0.2, plot=True)
-    example_vonmises(1., 0.5, plot=True)
+    example_truncnorm(-1.0, 1.0, 0.318, 0.2, plot=True)
+    example_wrapnorm(1.0, 0.2, plot=True)
+    example_vonmises(1.0, 0.5, plot=True)

examples/plot_skin_effect.py
@@ -7,10 +7,29 @@ from thermohl.power import olla
 
 
 def plot_skin_effect():
-    x = np.linspace(1., 50., 50)
+    x = np.linspace(1.0, 50.0, 50)
     D = 1.0
     R = 60 / x**2 / 1609.34  # Rdc from x and mile to meter conversion
-    ra = np.array([0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.76, 0.80, 0.82, 0.84, 0.86, 0.88, 0.90, 0.92])
+    ra = np.array(
+        [
+            0.0,
+            0.1,
+            0.2,
+            0.3,
+            0.4,
+            0.5,
+            0.6,
+            0.7,
+            0.76,
+            0.80,
+            0.82,
+            0.84,
+            0.86,
+            0.88,
+            0.90,
+            0.92,
+        ]
+    )
     dl = ra * D
     cl = cm.Spectral(np.linspace(0, 1, len(dl)))
 
@@ -18,25 +37,25 @@ def plot_skin_effect():
 
     plt.figure()
     for i, d in enumerate(dl):
-        di.update([('D', D), ('d', d), ('RDC20', R), ('f', 50.)])
+        di.update([("D", D), ("d", d), ("RDC20", R), ("f", 50.0)])
         jh = olla.JouleHeating(**di)
-        plt.plot(x, jh._ks(R), '-', c=cl[i], label='with $r_{in}/r_{out}$=%.2f' % (d,))
+        plt.plot(x, jh._ks(R), "-", c=cl[i], label="with $r_{in}/r_{out}$=%.2f" % (d,))
     plt.grid(True)
     plt.legend()
     plt.ylim([1.00, 1.15])
-    plt.title('_ks [from olla.Joule.Heating - computed with Bessel approx.]')
+    plt.title("_ks [from olla.Joule.Heating - computed with Bessel approx.]")
 
     plt.show()
 
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Test skin effect coefficient."""
 
     import matplotlib
 
-    matplotlib.use('TkAgg')
-    plt.close('all')
+    matplotlib.use("TkAgg")
+    plt.close("all")
 
     plot_skin_effect()