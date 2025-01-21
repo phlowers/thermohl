fix(DLR-1159): Add units tests (#11)
* fix(DLR-1159): Add units tests

- Reformatting code with Black
- Adding unit tests
- Adding docstring
- Renaming variables

Signed-off-by: SaintierFr <99645240+saintierfr@users.noreply.github.com>>
Signed-off-by: SaintierFr <99645240+SaintierFr@users.noreply.github.com>

examples/plot_sun.py
@@ -20,10 +20,10 @@ def _plot_seasons(ax=None, year=2001):
     aut = datetime.datetime(year, 9, 20, 12)
     win = datetime.datetime(year, 12, 21)
 
-    ax.axvline(x=spr, ymin=-23, ymax=+23, c='C0', ls='--', lw=1, label='spring')
-    ax.axvline(x=smr, ymin=-23, ymax=+23, c='C1', ls='--', lw=1, label='summer')
-    ax.axvline(x=aut, ymin=-23, ymax=+23, c='C2', ls='--', lw=1, label='autumn')
-    ax.axvline(x=win, ymin=-23, ymax=+23, c='C3', ls='--', lw=1, label='winter')
+    ax.axvline(x=spr, ymin=-23, ymax=+23, c="C0", ls="--", lw=1, label="spring")
+    ax.axvline(x=smr, ymin=-23, ymax=+23, c="C1", ls="--", lw=1, label="summer")
+    ax.axvline(x=aut, ymin=-23, ymax=+23, c="C2", ls="--", lw=1, label="autumn")
+    ax.axvline(x=win, ymin=-23, ymax=+23, c="C3", ls="--", lw=1, label="winter")
 
     return
 
@@ -31,15 +31,17 @@ def _plot_seasons(ax=None, year=2001):
 def plot_hour_angle():
     """Plot hour angle over time (one day)."""
 
-    dr = pd.date_range(datetime.datetime(2001, 4, 14, 20, 0, 0),
-                       datetime.datetime(2001, 4, 16, 4, 0, 0),
-                       freq='15min')
+    dr = pd.date_range(
+        datetime.datetime(2001, 4, 14, 20, 0, 0),
+        datetime.datetime(2001, 4, 16, 4, 0, 0),
+        freq="15min",
+    )
     plt.figure()
     ha = sun.hour_angle(dr.hour, minute=dr.minute)
     plt.plot(dr, ha / np.pi)
     plt.grid(True)
-    plt.xlabel('Time')
-    plt.ylabel('Hour angle / $\pi$')
+    plt.xlabel("Time")
+    plt.ylabel("Hour angle / $\pi$")
 
     plt.show()
 
@@ -49,18 +51,18 @@ def plot_hour_angle():
 def plot_solar_declination():
     """Plot solar declination over time (one year)."""
 
-    dr = pd.date_range(datetime.datetime(2000, 12, 15),
-                       datetime.datetime(2002, 1, 15),
-                       freq='D')
+    dr = pd.date_range(
+        datetime.datetime(2000, 12, 15), datetime.datetime(2002, 1, 15), freq="D"
+    )
 
     plt.figure()
     sd = np.rad2deg(sun.solar_declination(dr.month, dr.day))
-    plt.plot(dr, sd, '-', c='k')
+    plt.plot(dr, sd, "-", c="k")
     _plot_seasons()
     plt.grid(True)
     plt.legend()
-    plt.xlabel('Time')
-    plt.ylabel('Solar declination (deg)')
+    plt.xlabel("Time")
+    plt.ylabel("Solar declination (deg)")
 
     plt.show()
 
@@ -74,11 +76,11 @@ def plot_solar_altitude_1(only_positive=False):
 
     """
     lt = np.linspace(0, 0.5 * np.pi, 7)[:-1]
-    dr = pd.date_range(datetime.datetime(2001, 1, 1),
-                       datetime.datetime(2001, 12, 31),
-                       freq='D')
+    dr = pd.date_range(
+        datetime.datetime(2001, 1, 1), datetime.datetime(2001, 12, 31), freq="D"
+    )
     hl = np.linspace(0, 24, 13)[:-1]
-    cl = cm.magma(np.linspace(0., 1., len(hl) + 2))[1:-1]
+    cl = cm.magma(np.linspace(0.0, 1.0, len(hl) + 2))[1:-1]
 
     fig, ax = plt.subplots(nrows=2, ncols=len(lt))
     for j, lat in enumerate(lt):
@@ -86,15 +88,15 @@ def plot_solar_altitude_1(only_positive=False):
             for k, h in enumerate(hl):
                 sa = np.rad2deg(sun.solar_altitude(sg * lat, dr.month, dr.day, h))
                 if only_positive:
-                    sa = np.where(sa > 0., sa, 0.)
+                    sa = np.where(sa > 0.0, sa, 0.0)
                 sa = sa + np.where(h <= 12, 0, 1)
-                ax[i, j].plot(dr, sa, c=cl[k], label='At %02d:00' % (h,))
+                ax[i, j].plot(dr, sa, c=cl[k], label="At %02d:00" % (h,))
             ax[i, j].grid(True)
-            ax[i, j].set_title('At lat = %.1f' % (sg * np.rad2deg(lat),))
+            ax[i, j].set_title("At lat = %.1f" % (sg * np.rad2deg(lat),))
     for i in range(2):
-        ax[i, 0].set_ylabel('Solar altitude (deg)')
+        ax[i, 0].set_ylabel("Solar altitude (deg)")
     for j in range(len(lt)):
-        ax[-1, j].set_xlabel('Day of year')
+        ax[-1, j].set_xlabel("Day of year")
     ax[-1, -1].legend()
 
     plt.show()
@@ -112,7 +114,7 @@ def plot_solar_altitude_2(only_positive=False):
     mo = np.array(range(1, 13))
     dy = np.ones_like(mo) * 21
     hl = np.linspace(0, 24, 24 * 4)[:-1]
-    cl = cm.Spectral(np.linspace(0., 1., len(mo) + 2))[1:-1]
+    cl = cm.Spectral(np.linspace(0.0, 1.0, len(mo) + 2))[1:-1]
 
     fig, ax = plt.subplots(nrows=2, ncols=len(lt))
     for j, lat in enumerate(lt):
@@ -120,14 +122,14 @@ def plot_solar_altitude_2(only_positive=False):
             for k, m in enumerate(mo):
                 sa = np.rad2deg(sun.solar_altitude(sg * lat, mo[k], dy[k], hl))
                 if only_positive:
-                    sa = np.where(sa > 0., sa, 0.)
+                    sa = np.where(sa > 0.0, sa, 0.0)
                 ax[i, j].plot(hl, sa, c=cl[k], label=calendar.month_name[k + 1])
             ax[i, j].grid(True)
-            ax[i, j].set_title('At lat = %.1f' % (sg * np.rad2deg(lat),))
+            ax[i, j].set_title("At lat = %.1f" % (sg * np.rad2deg(lat),))
     for i in range(2):
-        ax[i, 0].set_ylabel('Solar altitude (deg)')
+        ax[i, 0].set_ylabel("Solar altitude (deg)")
     for j in range(len(lt)):
-        ax[-1, j].set_xlabel('Hour of day')
+        ax[-1, j].set_xlabel("Hour of day")
     ax[-1, -1].legend()
 
     plt.show()
@@ -142,11 +144,11 @@ def plot_solar_azimuth_1(only_positive=False):
 
     """
     lt = np.linspace(0, 0.5 * np.pi, 7)[:-1]
-    dr = pd.date_range(datetime.datetime(2001, 1, 1),
-                       datetime.datetime(2001, 12, 31),
-                       freq='D')
+    dr = pd.date_range(
+        datetime.datetime(2001, 1, 1), datetime.datetime(2001, 12, 31), freq="D"
+    )
     hl = np.linspace(0, 24, 13)[:-1]
-    cl = cm.magma(np.linspace(0., 1., len(hl) + 2))[1:-1]
+    cl = cm.magma(np.linspace(0.0, 1.0, len(hl) + 2))[1:-1]
 
     fig, ax = plt.subplots(nrows=2, ncols=len(lt))
     for j, lat in enumerate(lt):
@@ -154,15 +156,17 @@ def plot_solar_azimuth_1(only_positive=False):
             for k, h in enumerate(hl):
                 sa = np.rad2deg(sun.solar_azimuth(sg * lat, dr.month, dr.day, h))
                 if only_positive:
-                    sa = np.where(sun.solar_altitude(sg * lat, dr.month, dr.day, h) > 0., sa, 0.)
+                    sa = np.where(
+                        sun.solar_altitude(sg * lat, dr.month, dr.day, h) > 0.0, sa, 0.0
+                    )
                 sa = sa + np.where(h <= 12, 0, 1)
-                ax[i, j].plot(dr, sa, c=cl[k], label='At %02d:00' % (h,))
+                ax[i, j].plot(dr, sa, c=cl[k], label="At %02d:00" % (h,))
             ax[i, j].grid(True)
-            ax[i, j].set_title('At lat = %.1f' % (sg * np.rad2deg(lat),))
+            ax[i, j].set_title("At lat = %.1f" % (sg * np.rad2deg(lat),))
     for i in range(2):
-        ax[i, 0].set_ylabel('Solar azimuth (deg)')
+        ax[i, 0].set_ylabel("Solar azimuth (deg)")
     for j in range(len(lt)):
-        ax[-1, j].set_xlabel('Day of year')
+        ax[-1, j].set_xlabel("Day of year")
     ax[-1, -1].legend()
 
     plt.show()
@@ -180,7 +184,7 @@ def plot_solar_azimuth_2(only_positive=False):
     mo = np.array(range(1, 13))
     dy = np.ones_like(mo) * 21
     hl = np.linspace(0, 24, 24 * 4)[:-1]
-    cl = cm.Spectral(np.linspace(0., 1., len(mo) + 2))[1:-1]
+    cl = cm.Spectral(np.linspace(0.0, 1.0, len(mo) + 2))[1:-1]
 
     fig, ax = plt.subplots(nrows=2, ncols=len(lt))
     for j, lat in enumerate(lt):
@@ -188,14 +192,16 @@ def plot_solar_azimuth_2(only_positive=False):
             for k, m in enumerate(mo):
                 sa = np.rad2deg(sun.solar_azimuth(sg * lat, mo[k], dy[k], hl))
                 if only_positive:
-                    sa = np.where(sun.solar_altitude(sg * lat, mo[k], dy[k], hl) > 0., sa, 0.)
+                    sa = np.where(
+                        sun.solar_altitude(sg * lat, mo[k], dy[k], hl) > 0.0, sa, 0.0
+                    )
                 ax[i, j].plot(hl, sa, c=cl[k], label=calendar.month_name[k + 1])
             ax[i, j].grid(True)
-            ax[i, j].set_title('At lat = %.1f' % (sg * np.rad2deg(lat),))
+            ax[i, j].set_title("At lat = %.1f" % (sg * np.rad2deg(lat),))
     for i in range(2):
-        ax[i, 0].set_ylabel('Solar azimuth (deg)')
+        ax[i, 0].set_ylabel("Solar azimuth (deg)")
     for j in range(len(lt)):
-        ax[-1, j].set_xlabel('Hour of day')
+        ax[-1, j].set_xlabel("Hour of day")
     ax[-1, -1].legend()
 
     plt.show()
@@ -203,11 +209,11 @@ def plot_solar_azimuth_2(only_positive=False):
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import matplotlib
 
-    matplotlib.use('TkAgg')
-    plt.close('all')
+    matplotlib.use("TkAgg")
+    plt.close("all")
 
     plot_hour_angle()
     plot_solar_declination()

src/thermohl/solver/base.py
@@ -6,6 +6,7 @@ from typing import Tuple, Type, Any, Optional, KeysView, Dict
 
 import numpy as np
 import pandas as pd
+from numpy import ndarray
 
 from thermohl import (
     floatArrayLike,
@@ -108,7 +109,17 @@ class Args:
         self.__dict__[key] = value
 
     def max_len(self) -> int:
-        """."""
+        """
+        Calculate the maximum length of the values in the dictionary.
+
+        This method iterates over all keys in the dictionary and determines the maximum length
+        of the values associated with those keys.
+        If a value is not of a type that has a length, it is ignored.
+
+        Returns:
+            int: The maximum length of the values in the dictionary. If the dictionary is empty
+            or all values are of types that do not have a length, the method returns 1.
+        """
         n = 1
         for k in self.keys():
             try:
@@ -118,7 +129,20 @@ class Args:
         return n
 
     def extend_to_max_len(self) -> None:
-        """."""
+        """
+        Extend all elements in the dictionary to the maximum length.
+
+        This method iterates over all keys in the dictionary and checks if the
+        corresponding value is a numpy ndarray. If it is, it checks if its length
+        matches the maximum length obtained from the `max_len` method.
+        If the length matches, it creates a copy of the array.
+        If the length does not match or for non-ndarray values, it creates
+        a new numpy array of the maximum length, filled with the original value
+        and having the same data type.
+
+        Returns:
+            None
+        """
         n = self.max_len()
         for k in self.keys():
             if isinstance(self[k], np.ndarray):
@@ -133,6 +157,13 @@ class Args:
                 self[k] = self[k] * np.ones((n,), dtype=t)
 
     def compress(self) -> None:
+        """
+        Compresses the values in the dictionary by replacing numpy arrays with a
+        single unique value if all elements in the array are the same.
+
+        Returns:
+            None
+        """
         for k in self.keys():
             if isinstance(self[k], np.ndarray):
                 u = np.unique(self[k])
@@ -178,14 +209,14 @@ class Solver(ABC):
             )
 
     def __init__(
-            self,
-            dic: Optional[dict[str, Any]] = None,
-            joule: Type[PowerTerm] = PowerTerm,
-            solar: Type[PowerTerm] = PowerTerm,
-            convective: Type[PowerTerm] = PowerTerm,
-            radiative: Type[PowerTerm] = PowerTerm,
-            precipitation: Type[PowerTerm] = PowerTerm,
-    ):
+        self,
+        dic: Optional[dict[str, Any]] = None,
+        joule: Type[PowerTerm] = PowerTerm,
+        solar: Type[PowerTerm] = PowerTerm,
+        convective: Type[PowerTerm] = PowerTerm,
+        radiative: Type[PowerTerm] = PowerTerm,
+        precipitation: Type[PowerTerm] = PowerTerm,
+    ) -> None:
         """Create a Solver object.
 
         Parameters
@@ -215,7 +246,6 @@ class Solver(ABC):
         self.rc = radiative(**self.args.__dict__)
         self.pc = precipitation(**self.args.__dict__)
         self.args.compress()
-        return
 
     def update(self) -> None:
         self.args.extend_to_max_len()
@@ -225,15 +255,14 @@ class Solver(ABC):
         self.rc.__init__(**self.args.__dict__)
         self.pc.__init__(**self.args.__dict__)
         self.args.compress()
-        return
 
     def balance(self, T: floatArrayLike) -> floatArrayLike:
         return (
-                self.jh.value(T)
-                + self.sh.value(T)
-                - self.cc.value(T)
-                - self.rc.value(T)
-                - self.pc.value(T)
+            self.jh.value(T)
+            + self.sh.value(T)
+            - self.cc.value(T)
+            - self.rc.value(T)
+            - self.pc.value(T)
         )
 
     @abstractmethod
@@ -249,51 +278,69 @@ class Solver(ABC):
         raise NotImplementedError
 
 
-def _reshape1d(v: numberArrayLike, n: int) -> numberArray:
-    """Reshape input v in size (n,) if possible."""
-    try:
-        l = len(v)
-        if l == 1:
-            w = v * np.ones(n, dtype=v.dtype)
-        else:
-            raise ValueError("Uncompatible size")
-    except AttributeError:
-        w = v * np.ones(n, dtype=type(v))
-    return w
+def reshape(input_array: numberArrayLike, nb_row: int, nb_columns: int) -> numberArray:
+    """
+    Reshape the input array to the specified dimensions (nr, nc) if possible.
 
+    Args:
+        input_array (numberArrayLike): Input array to be reshaped.
+        nb_row (int): Desired number of rows for the reshaped array.
+        nb_columns (int): Desired number of columns for the reshaped array.
 
-def reshape(v: numberArrayLike, nr: int, nc: int) -> numberArray:
-    """Reshape input v in size (nr, nc) if possible."""
+    Returns:
+        numberArray: Reshaped array of size (nb_row, nb_columns). If reshaping is not possible,
+            returns an array filled with the input_value repeated to fill the dimension (nb_row, nb_columns).
+
+    Raises:
+        AttributeError: If the input_array has an invalid shape that cannot be reshaped.
+    """
+    reshaped_array = ndarray
     try:
-        s = v.shape
-        if len(s) == 1:
-            if nr == s[0]:
-                w = np.column_stack(nc * (v,))
-            elif nc == s[0]:
-                w = np.vstack(nr * (v,))
-        elif len(s) == 0:
+        input_shape = input_array.shape
+        if len(input_shape) == 1:
+            if nb_row == input_shape[0]:
+                reshaped_array = np.column_stack(nb_columns * (input_array,))
+            elif nb_columns == input_shape[0]:
+                reshaped_array = np.vstack(nb_row * (input_array,))
+        elif len(input_shape) == 0:
             raise AttributeError()
         else:
-            w = np.reshape(v, (nr, nc))
+            reshaped_array = np.reshape(input_array, (nb_row, nb_columns))
     except AttributeError:
-        w = v * np.ones((nr, nc), dtype=type(v))
-    return w
+        reshaped_array = input_array * np.ones(
+            (nb_row, nb_columns), dtype=type(input_array)
+        )
+    return reshaped_array
 
 
 def _set_dates(
-        month: floatArrayLike,
-        day: floatArrayLike,
-        hour: floatArrayLike,
-        t: floatArray,
-        n: int,
+    month: floatArrayLike,
+    day: floatArrayLike,
+    hour: floatArrayLike,
+    time: floatArray,
+    n: int,
 ) -> Tuple[intArray, intArray, floatArray]:
-    """Set months, days and hours as 2D arrays.
+    """
+    Set months, days and hours as 2D arrays.
 
     This function is used in transient temperature computations. Inputs month,
     day and hour are floats or 1D arrays of size n; input t is a time vector of
     size N with evaluation times in seconds. It sets arrays months, days and
     hours, of size (N, n) such that
         months[i, j] = datetime(month[j], day[j], hour[j]) + t[i] .
+
+    Args:
+        month (floatArrayLike): Array of floats or float representing the months.
+        day (floatArrayLike): Array of floats or float representing the days.
+        hour (floatArrayLike): Array of floats or float representing the hours.
+        time (floatArray): Array of floats representing the time vector in seconds.
+        n (int): Size of the input arrays month, day, and hour.
+
+    Returns:
+    Tuple[intArray, intArray, floatArray]:
+        - months (intArray): 2D array of shape (N, n) with month values.
+        - days (intArray): 2D array of shape (N, n) with day values.
+        - hours (floatArray): 2D array of shape (N, n) with hour values.
     """
     oi = np.ones((n,), dtype=int)
     of = np.ones((n,), dtype=float)
@@ -301,27 +348,30 @@ def _set_dates(
     day2 = day * oi
     hour2 = hour * of
 
-    N = len(t)
+    N = len(time)
     months = np.zeros((N, n), dtype=int)
     days = np.zeros((N, n), dtype=int)
     hours = np.zeros((N, n), dtype=float)
 
     td = np.array(
         [datetime.timedelta()]
-        + [datetime.timedelta(seconds=t[i] - t[i - 1]) for i in range(1, N)]
+        + [
+            datetime.timedelta(seconds=float(time[i] - time[i - 1]))
+            for i in range(1, N)
+        ]
     )
 
     for j in range(n):
         hj = int(np.floor(hour2[j]))
-        dj = datetime.timedelta(seconds=3600.0 * (hour2[j] - hj))
+        dj = datetime.timedelta(seconds=float(3600.0 * (hour2[j] - hj)))
         t0 = datetime.datetime(year=2000, month=month2[j], day=day2[j], hour=hj) + dj
         ts = pd.Series(t0 + td)
         months[:, j] = ts.dt.month
         days[:, j] = ts.dt.day
         hours[:, j] = (
-                ts.dt.hour
-                + ts.dt.minute / 60.0
-                + (ts.dt.second + ts.dt.microsecond * 1.0e-06) / 3600.0
+            ts.dt.hour
+            + ts.dt.minute / 60.0
+            + (ts.dt.second + ts.dt.microsecond * 1.0e-06) / 3600.0
         )
 
     return months, days, hours

src/thermohl/uncertainties.py
@@ -183,7 +183,9 @@ def _steady_uncertainties(
     #  all to max_len size
     n = utils.dict_max_len(s.dc)
     dc = utils.extend_to_max_len(s.dc, n)
-    Tmax = tmax * np.ones(n, )
+    Tmax = tmax * np.ones(
+        n,
+    )
 
     # add missing uncertainties parameters
     du = utils.add_default_uncertainties(u)
@@ -417,7 +419,9 @@ def sensitivity(
     #  all to max_len size
     n = utils.dict_max_len(s.dc)
     dc = utils.extend_to_max_len(s.dc, n)
-    Tmax = tmax * np.ones(n, )
+    Tmax = tmax * np.ones(
+        n,
+    )
 
     # add missing uncertainties parameters
     du = utils.add_default_uncertainties(u)

test/test_ieee.py
@@ -11,49 +11,59 @@ def test_compare_powers():
     # there are a lot of rounding in the standard guide, hence the relatively
     # large tolerances used in our tests ...
 
-    dic['ws'] = 0.61
-    dic['wa'] = 0.
-    dic['epsilon'] = 0.8
-    dic['alpha'] = 0.8
-    dic['Ta'] = 40.
-    dic['THigh'] = 75.
-    dic['TLow'] = 25.
-    dic['RDCHigh'] = 8.688E-05
-    dic['RDCLow'] = 7.283E-05
-    dic['azm'] = 90.
-    dic['lat'] = 30.
-    dic['tb'] = 0.
-    dic['alt'] = 0.
-    dic['D'] = 28.14 * 1.0E-03
-    dic['d'] = 10.4 * 1.0E-03
-    dic['month'] = 6
-    dic['day'] = 10
-    dic['hour'] = 11.
+    dic["ws"] = 0.61
+    dic["wa"] = 0.0
+    dic["epsilon"] = 0.8
+    dic["alpha"] = 0.8
+    dic["Ta"] = 40.0
+    dic["THigh"] = 75.0
+    dic["TLow"] = 25.0
+    dic["RDCHigh"] = 8.688e-05
+    dic["RDCLow"] = 7.283e-05
+    dic["azm"] = 90.0
+    dic["lat"] = 30.0
+    dic["tb"] = 0.0
+    dic["alt"] = 0.0
+    dic["D"] = 28.14 * 1.0e-03
+    dic["d"] = 10.4 * 1.0e-03
+    dic["month"] = 6
+    dic["day"] = 10
+    dic["hour"] = 11.0
 
-    T = 100.
+    T = 100.0
 
-    assert np.isclose(thermohl.power.ieee.ConvectiveCooling(**dic).value(T), 81.93, rtol=0.002)
-    assert np.isclose(thermohl.power.ieee.RadiativeCooling(**dic).value(T), 39.1, rtol=0.001)
-    assert np.isclose(thermohl.power.ieee.SolarHeating(**dic).value(T), 22.44, rtol=0.001)
+    assert np.isclose(
+        thermohl.power.ieee.ConvectiveCooling(**dic).value(T), 81.93, rtol=0.002
+    )
+    assert np.isclose(
+        thermohl.power.ieee.RadiativeCooling(**dic).value(T), 39.1, rtol=0.001
+    )
+    assert np.isclose(
+        thermohl.power.ieee.SolarHeating(**dic).value(T), 22.44, rtol=0.001
+    )
     jh = thermohl.power.ieee.JouleHeating(**dic)
-    assert np.isclose(jh._rdc(T), 9.390E-05, rtol=1.0E-09)
+    assert np.isclose(jh._rdc(T), 9.390e-05, rtol=1.0e-09)
 
     # additional debug
     thermohl.power.ieee.SolarHeating(**dic).value(T)
 
     from thermohl import sun
 
-    sd = sun.solar_declination(dic['month'], dic['day'])
+    sd = sun.solar_declination(dic["month"], dic["day"])
     assert np.isclose(np.rad2deg(sd), 23.0, rtol=0.001)
 
-    ha = sun.hour_angle(dic['hour'], minute=0., second=0.)
-    assert np.isclose(np.rad2deg(ha), -15.)
+    ha = sun.hour_angle(dic["hour"], minute=0.0, second=0.0)
+    assert np.isclose(np.rad2deg(ha), -15.0)
 
-    sa = sun.solar_altitude(np.deg2rad(dic['lat']), dic['month'], dic['day'], dic['hour'])
+    sa = sun.solar_altitude(
+        np.deg2rad(dic["lat"]), dic["month"], dic["day"], dic["hour"]
+    )
     assert np.isclose(np.rad2deg(sa), 74.8, rtol=0.002)
 
-    sz = sun.solar_azimuth(np.deg2rad(dic['lat']), dic['month'], dic['day'], dic['hour'])
-    np.isclose(np.rad2deg(sz), 114., rtol=0.001)
+    sz = sun.solar_azimuth(
+        np.deg2rad(dic["lat"]), dic["month"], dic["day"], dic["hour"]
+    )
+    np.isclose(np.rad2deg(sz), 114.0, rtol=0.001)
 
-    th = np.arccos(np.cos(sa) * np.cos(sz - dic['azm']))
+    th = np.arccos(np.cos(sa) * np.cos(sz - dic["azm"]))
     np.isclose(np.rad2deg(th), 76.1, rtol=0.02)

test/test_shape.py
@@ -6,8 +6,8 @@ from thermohl import solver
 
 def _solvers():
     li = []
-    for ht in ['1t', '3t']:
-        for m in ['cner', 'cigre', 'ieee', 'olla']:
+    for ht in ["1t", "3t"]:
+        for m in ["cner", "cigre", "ieee", "olla"]:
             li.append(solver._factory(dic=None, heateq=ht, model=m))
     return li
 
@@ -26,8 +26,12 @@ def _traargs(s: solver.Solver, ds: pd.DataFrame, t, I):
     if isinstance(s, solver.Solver1T):
         a = dict(time=t, T0=ds[solver.Solver.Names.temp].values, transit=I)
     elif isinstance(s, solver.Solver3T):
-        a = dict(time=t, transit=I, Ts0=ds[solver.Solver.Names.tsurf].values,
-                 Tc0=ds[solver.Solver.Names.tcore].values)
+        a = dict(
+            time=t,
+            transit=I,
+            Ts0=ds[solver.Solver.Names.tsurf].values,
+            Tc0=ds[solver.Solver.Names.tcore].values,
+        )
     else:
         raise NotImplementedError
     return a
@@ -38,9 +42,9 @@ def test_power_default():
     for s in _solvers():
         for p in [s.jh, s.sh, s.cc, s.rc, s.pc]:
             p.__init__(**s.args.__dict__)
-            assert np.isscalar(p.value(0.))
-            assert p.value(np.array([0.])).shape == (1,)
-            assert p.value(np.array([0., 10.])).shape == (2,)
+            assert np.isscalar(p.value(0.0))
+            assert p.value(np.array([0.0])).shape == (1,)
+            assert p.value(np.array([0.0, 10.0])).shape == (2,)
 
 
 def test_power_1d():
@@ -48,14 +52,14 @@ def test_power_1d():
     n = 61
     for s in _solvers():
         d = s.args.__dict__.copy()
-        d['I'] = np.linspace(0., +999., n)
-        d['alpha'] = np.linspace(0.5, 0.9, n)
-        d['Ta'] = np.linspace(-10., +50., n)
+        d["I"] = np.linspace(0.0, +999.0, n)
+        d["alpha"] = np.linspace(0.5, 0.9, n)
+        d["Ta"] = np.linspace(-10.0, +50.0, n)
         for p in [s.jh, s.sh, s.cc, s.rc, s.pc]:
             p.__init__(**d)
-            v = p.value(0.)
+            v = p.value(0.0)
             assert np.isscalar(v) or v.shape == (n,)
-            v = p.value(np.array([0.]))
+            v = p.value(np.array([0.0]))
             assert v.shape == (1,) or v.shape == (n,)
             assert p.value(np.linspace(-10, +50, n)).shape == (n,)
 
@@ -85,7 +89,7 @@ def test_steady_1d_mix():
     n = 61
     for s in _solvers():
         s.args.Ta = np.linspace(-10, +50, n)
-        s.args.I = np.array([199.])
+        s.args.I = np.array([199.0])
         s.update()
         t = s.steady_temperature()
         a = _ampargs(s, t)
@@ -103,13 +107,13 @@ def test_transient_0():
         a = _traargs(s, ds, t, I)
 
         r = s.transient_temperature(**a)
-        assert len(r.pop('time')) == len(t)
+        assert len(r.pop("time")) == len(t)
         for k in r.keys():
             assert r[k].shape == (len(t),)
 
-        r = s.transient_temperature(**{**a, 'return_power': True})
+        r = s.transient_temperature(**{**a, "return_power": True})
 
-        assert len(r.pop('time')) == len(t)
+        assert len(r.pop("time")) == len(t)
         for k in r.keys():
             assert r[k].shape == (len(t),)
 
@@ -127,11 +131,11 @@ def test_transient_1():
         a = _traargs(s, ds, t, I)
 
         r = s.transient_temperature(**a)
-        assert len(r.pop('time')) == len(t)
+        assert len(r.pop("time")) == len(t)
         for k in r.keys():
             assert r[k].shape == (len(t), n)
 
-        r = s.transient_temperature(**{**a, 'return_power': True})
-        assert len(r.pop('time')) == len(t)
+        r = s.transient_temperature(**{**a, "return_power": True})
+        assert len(r.pop("time")) == len(t)
         for k in r.keys():
             assert r[k].shape == (len(t), n)