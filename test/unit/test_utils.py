018e7109f6cd6506d69c6ba28be5bf7b3b7c9928
SaintierFr <99645240+SaintierFr@users.noreply.github.com>
2025-02-06 11:30:49 +0100

    feat(DLR-1190): Suppress pyntb dependency (#13)
    * feat(DLR-1190): Suppress pyntb dependency
    
    - Update pyproject.toml
    - Add usefull functions from pyntb
    - Add unit tests
    
    Signed-off-by: SAINTIER Francois <francois.saintier@rte-france.com>
    
    * feat(DLR-1190): Suppress pyntb dependency
    
    - Update pyproject.toml
    - Add copyright
    - Add usefull functions from pyntb
    - Add unit tests
    
    Signed-off-by: SAINTIER Francois <francois.saintier@rte-france.com>
    
    ---------
    
    Signed-off-by: SAINTIER Francois <francois.saintier@rte-france.com>
    Signed-off-by: SaintierFr <99645240+SaintierFr@users.noreply.github.com>

---
+++ test/unit/test_utils.py
@@ -0,0 +1,160 @@
+import numpy as np
+
+from thermohl.utils import bisect_v, quasi_newton_2d
+
+_nprs = 3141592654
+
+
+def test_bisect():
+    np.random.seed(_nprs)
+    size = 99
+    tol = 1.0e-09
+
+    c = np.linspace(1, size, size) + np.random.randn(size)
+    c = np.abs(c)
+
+    def fun(x):
+        return x**2 - c
+
+    r0 = np.sqrt(c)
+
+    x0, err = bisect_v(
+        fun,
+        0.0,
+        np.sqrt(np.max(c)) * 1.1,
+        (size,),
+        print_err=False,
+        tol=tol,
+        maxiter=99,
+    )
+
+    assert np.max(np.abs(x0 - r0) <= np.minimum(tol, err))
+
+
+def test_bisect_scalar():
+    def f(x):
+        return x**2 - 2
+
+    tol = 1e-6
+    x0, err = bisect_v(f, a=0, b=2, shape=1, tol=tol)
+    np.testing.assert_allclose(x0, np.sqrt(2), atol=tol)
+
+
+def test_bisect_vector():
+    def f(x):
+        return np.array([x[0] ** 2 - 2, x[1] ** 3 - 2])
+        # Not the best way to use bisect_v, but just for testing.
+
+    tol = 1e-6
+    x0, err = bisect_v(f, a=0, b=2, shape=(2,), tol=tol)
+    np.testing.assert_allclose(x0, np.array([np.sqrt(2), np.cbrt(2)]), atol=tol)
+
+
+def test_bisect_array():
+    c = np.arange(27).reshape(3, 3, 3)
+
+    def f(x):
+        return x**2 - c
+
+    tol = 1e-6
+    x0, err = bisect_v(f, a=0, b=30, shape=c.shape, tol=tol)
+    np.testing.assert_allclose(x0, np.sqrt(c), atol=tol)
+
+
+#
+def test_quasi_newton_2d_convergence():
+    np.random.seed(_nprs)
+    size = 10
+    tol = 1.0e-12
+
+    a = np.abs(1 + np.random.randn(size))
+    b = np.abs(1 + np.random.randn(size))
+
+    def f1(x, y):
+        return y - a * x**2
+
+    def f2(x, y):
+        return y - b * x**3
+
+    xg = np.ones((size,))
+    yg = np.ones((size,))
+    x, y, count, err = quasi_newton_2d(
+        f1=f1,
+        f2=f2,
+        x0=xg,
+        y0=yg,
+        relative_tolerance=tol,
+        max_iterations=999,
+        delta_x=1.0e-09,
+        delta_y=1.0e-09,
+    )
+
+    assert np.logical_and(
+        np.max(np.abs(f1(x, y))) < tol, np.max(np.abs(f2(x, y))) < tol
+    )
+    assert count == 999
+
+
+def test_quasi_newton_2d_no_convergence():
+    np.random.seed(_nprs)
+    size = 10
+    tol = 1.0e-12
+
+    a = np.abs(1 + np.random.randn(size))
+    b = np.abs(1 + np.random.randn(size))
+
+    def f1(x, y):
+        return y - a * x**2
+
+    def f2(x, y):
+        return y - b * x**3
+
+    xg = np.ones((size,))
+    yg = np.ones((size,))
+    x, y, count, err = quasi_newton_2d(
+        f1=f1,
+        f2=f2,
+        x0=xg,
+        y0=yg,
+        relative_tolerance=tol,
+        max_iterations=1,  # Set max iterations to 1 to force no convergence
+        delta_x=1.0e-09,
+        delta_y=1.0e-09,
+    )
+
+    assert count == 1
+    assert np.logical_or(
+        np.max(np.abs(f1(x, y))) >= tol, np.max(np.abs(f2(x, y))) >= tol
+    )
+
+
+def test_quasi_newton_2d_large_system():
+    np.random.seed(_nprs)
+    size = 1000
+    tol = 1.0e-12
+
+    a = np.abs(1 + np.random.randn(size))
+    b = np.abs(1 + np.random.randn(size))
+
+    def f1(x, y):
+        return y - a * x**2
+
+    def f2(x, y):
+        return y - b * x**3
+
+    xg = np.ones((size,))
+    yg = np.ones((size,))
+    x, y, count, err = quasi_newton_2d(
+        f1=f1,
+        f2=f2,
+        x0=xg,
+        y0=yg,
+        relative_tolerance=tol,
+        max_iterations=999,
+        delta_x=1.0e-09,
+        delta_y=1.0e-09,
+    )
+
+    assert np.logical_and(
+        np.max(np.abs(f1(x, y))) < tol, np.max(np.abs(f2(x, y))) < tol
+    )
