cc7b206d0f4774017b348d5bf7e7bbfbb129793c
SaintierFr <99645240+SaintierFr@users.noreply.github.com>
2025-03-03 09:20:40 +0100

    feat(DLR-1235): Add headers with copyright (#16)
    - Add REUSE template header and License file
    - Add reuse action
    - Generate licence and copyright headers on all project files

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -1,3 +1,10 @@
+# SPDX-FileCopyrightText: 2025 RTE (https://www.rte-france.com)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
 import numpy as np
 
 from thermohl.utils import bisect_v, quasi_newton_2d
