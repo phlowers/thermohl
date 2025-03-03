66d8e8a1de167dc16f4587c020cac74a99d0adb4
SaintierFr <99645240+SaintierFr@users.noreply.github.com>
2025-03-03 09:20:40 +0100

    feat(DLR-1235): Add headers with copyright (#16)
    - Add REUSE template header and License file
    - Add reuse action
    - Generate licence and copyright headers on all project files
    
    Signed-off-by: SAINTIER Francois <francois.saintier@rte-france.com>

--- src/thermohl/sun.py
+++ src/thermohl/sun.py
@@ -1,3 +1,10 @@
+# SPDX-FileCopyrightText: 2025 RTE (https://www.rte-france.com)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
 """Utility functions to compute different sun positions from a point on earth.
 
     These positions usually depend on the point latitude and the time. The sun
