1e6f74e39867f54f93657b0e8ff2ead6b85d9f37
SaintierFr <99645240+SaintierFr@users.noreply.github.com>
2025-03-03 09:20:40 +0100

    feat(DLR-1235): Add headers with copyright (#16)
    - Add REUSE template header and License file
    - Add reuse action
    - Generate licence and copyright headers on all project files
    
    Signed-off-by: SaintierFr <99645240+SaintierFr@users.noreply.github.com>

--- examples/example_solver_intensity.py
+++ examples/example_solver_intensity.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2025 RTE (https://www.rte-france.com)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
 # -*- coding: utf-8 -*-
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
