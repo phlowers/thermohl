9b56be3d407ee01b3f0459619cd1f6ce64aaa132
fbaraket <fatma.baraket@mews-labs.com>
2025-07-17 17:58:13 +0200

    update example
    Signed-off-by: fbaraket <fatma.baraket@mews-labs.com>

--- examples/example_steady_1t.py
+++ examples/example_steady_1t.py
@@ -7,34 +7,13 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 from thermohl import solver
 
-
-
-if __name__ == "__main__":
-    import matplotlib
-
-    matplotlib.use("TkAgg")
-    plt.close("all")
-
-    # Generate input dict (for the sake of simplicity, only a few inputs are
-    # used, the rest is filled with default values).
-    dic = dict(
-        lat=46.1,
-        alt=123.,
-        azm=31.,
-        month=6,
-        day=20,
-        hour=np.linspace(0., 23., 24),
-        I=np.array([400. for i in range(12)] + [700. for i in range(12)]),
-    )
-
+def example_solver1t(dic):
     # create solver with ieee power terms and 1t heat equation; other options
     # available are solver.cigre, solver.olla and solver.rte for power terms
     slvr = solver.ieee(dic, heateq="1t")
-
 
     # test 1: compute temperature (using transit from dic["I"])
     dtemp = slvr.steady_temperature()
@@ -55,3 +34,25 @@ if __name__ == "__main__":
         ax[i].legend()
     ax[2].set_xlabel("Hour")
     plt.show()
+
+
+if __name__ == "__main__":
+    import matplotlib
+
+    matplotlib.use("TkAgg")
+    plt.close("all")
+
+    # Generate input dict (for the sake of simplicity, only a few inputs are
+    # used, the rest is filled with default values).
+    dic = dict(
+        lat=46.1,
+        alt=123.,
+        azm=31.,
+        month=6,
+        day=20,
+        hour=np.linspace(0., 23., 24),
+        I=np.array([400. for i in range(12)] + [700. for i in range(12)]),
+    )
+    example_solver1t(dic)
+
+
