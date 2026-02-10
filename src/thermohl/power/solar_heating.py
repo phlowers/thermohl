12a152398355b38c7f1bd0d280b37a3ec01cd4ca
QUINAOU Aimee <aimee.quinaou@rte-france.com>
2026-02-10 11:39:33 +0100

    Rename trb -> turbidity
    Signed-off-by: QUINAOU Aimee <aimee.quinaou@rte-france.com>

--- src/thermohl/power/solar_heating.py
+++ src/thermohl/power/solar_heating.py
@@ -27,7 +27,7 @@ class _SRad:
         self.indus = indus
 
     def atmosphere_turbidity(
-        self, x: floatArrayLike, trb: Optional[floatArrayLike] = 0.0
+        self, x: floatArrayLike, turbidity: Optional[floatArrayLike] = 0.0
     ) -> floatArrayLike:
         """Compute coefficient for atmosphere turbidity.
         This method calculates the atmospheric turbidity coefficient using a polynomial
@@ -37,19 +37,19 @@ class _SRad:
 
         Args:
             x (float | numpy.ndarray): Solar altitude in degrees.
-            trb (float | numpy.ndarray): Atmospheric turbidity factor (0 for clean air, 1 for industrial air).
+            turbidity (float | numpy.ndarray): Atmospheric turbidity factor (0 for clean air, 1 for industrial air).
 
         Returns:
             float | numpy.ndarray: Coefficient for atmospheric turbidity.
         """
-        clean_weight = 1.0 - trb
-        coeff_6 = clean_weight * self.clean[6] + trb * self.indus[6]
-        coeff_5 = clean_weight * self.clean[5] + trb * self.indus[5]
-        coeff_4 = clean_weight * self.clean[4] + trb * self.indus[4]
-        coeff_3 = clean_weight * self.clean[3] + trb * self.indus[3]
-        coeff_2 = clean_weight * self.clean[2] + trb * self.indus[2]
-        coeff_1 = clean_weight * self.clean[1] + trb * self.indus[1]
-        coeff_0 = clean_weight * self.clean[0] + trb * self.indus[0]
+        clean_weight = 1.0 - turbidity
+        coeff_6 = clean_weight * self.clean[6] + turbidity * self.indus[6]
+        coeff_5 = clean_weight * self.clean[5] + turbidity * self.indus[5]
+        coeff_4 = clean_weight * self.clean[4] + turbidity * self.indus[4]
+        coeff_3 = clean_weight * self.clean[3] + turbidity * self.indus[3]
+        coeff_2 = clean_weight * self.clean[2] + turbidity * self.indus[2]
+        coeff_1 = clean_weight * self.clean[1] + turbidity * self.indus[1]
+        coeff_0 = clean_weight * self.clean[0] + turbidity * self.indus[0]
         return (
             coeff_6 * x**6
             + coeff_5 * x**5
@@ -65,7 +65,7 @@ class _SRad:
         latitude_deg: floatArrayLike,
         altitude: floatArrayLike,
         azimuth: floatArrayLike,
-        trb: floatArrayLike,
+        turbidity: floatArrayLike,
         month: intArrayLike,
         day: intArrayLike,
         hour: floatArrayLike,
@@ -78,7 +78,7 @@ class _SRad:
         )
         altitude_factor = 1.0 + 1.148e-04 * altitude - 1.108e-08 * altitude**2
         clearness_factor = self.atmosphere_turbidity(
-            np.rad2deg(solar_altitude_rad), trb
+            np.rad2deg(solar_altitude_rad), turbidity
         )
         solar_irradiance = (
             altitude_factor * clearness_factor * np.sin(incidence_angle_rad)
