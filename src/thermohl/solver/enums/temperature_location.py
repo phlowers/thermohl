1a3751f3ee012f56a02a51960f92232a410fd443
Olivier Pigeon <olivier.pigeon@rte-france.com>
2026-02-13 11:47:31 +0100

    feat(#2): Create enums for solver types
    Delete Solver.Names and create enums instead to make the code more robust
    
    BREAKING CHANGE: The solvers must be called with enums instead of strings from now
    
    Signed-off-by: Olivier Pigeon <olivier.pigeon@rte-france.com>

---
+++ src/thermohl/solver/enums/temperature_location.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2025 RTE (https://www.rte-france.com)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
+from enum import Enum
+
+
+class TemperatureLocation(Enum):
+    SURFACE = "t_surf"
+    AVERAGE = "t_avg"
+    CORE = "t_core"
