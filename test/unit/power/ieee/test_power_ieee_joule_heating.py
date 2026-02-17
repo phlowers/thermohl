3241ff52970e9a906f20711fef625d9e3cf29583
ai-qui <184963772+ai-qui@users.noreply.github.com>
2026-02-17 10:08:14 +0100

    Format files
    Signed-off-by: ai-qui <184963772+ai-qui@users.noreply.github.com>

--- test/unit/power/ieee/test_power_ieee_joule_heating.py
+++ test/unit/power/ieee/test_power_ieee_joule_heating.py
@@ -104,8 +104,10 @@ joule_heating_instances = [
 )
 def test_rdc_scalar_temperature(joule_heating):
     conductor_temperature_c = 50.0
-    expected = joule_heating.linear_resistance_temp_low_ohm_m + joule_heating.temp_coeff_linear * (
-        conductor_temperature_c - joule_heating.temp_low_c
+    expected = (
+        joule_heating.linear_resistance_temp_low_ohm_m
+        + joule_heating.temp_coeff_linear
+        * (conductor_temperature_c - joule_heating.temp_low_c)
     )
 
     result = joule_heating._rdc(conductor_temperature_c)
@@ -124,8 +126,10 @@ def test_rdc_scalar_temperature(joule_heating):
 )
 def test_rdc_array_temperature(joule_heating):
     conductor_temperature_c = np.array([30.0, 40.0])
-    expected = joule_heating.linear_resistance_temp_low_ohm_m + joule_heating.temp_coeff_linear * (
-        conductor_temperature_c - joule_heating.temp_low_c
+    expected = (
+        joule_heating.linear_resistance_temp_low_ohm_m
+        + joule_heating.temp_coeff_linear
+        * (conductor_temperature_c - joule_heating.temp_low_c)
     )
 
     result = joule_heating._rdc(conductor_temperature_c)
