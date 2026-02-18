Fix ampacity computation by 1T solver under hot weather
	Fix bisect_v: return np.nan when the condition
	f(lower_bound) < 0 < f(upper_bound) is not met.

	Therefore the intensity computed by 1T solver when
	the weather is so hot that there's no solution, is np.nan,
	just like with the other solvers.

Signed-off-by: ai-qui <184963772+ai-qui@users.noreply.github.com>

test/unit/test_utils.py
@@ -60,7 +60,7 @@ def test_bisect_vector():
 
 
 def test_bisect_array():
-    c = np.arange(27).reshape(3, 3, 3)
+    c = np.arange(-1, 26).reshape(3, 3, 3)
 
     def f(x):
         return x**2 - c
@@ -69,7 +69,32 @@ def test_bisect_array():
     x0, err = bisect_v(
         f, lower_bound=0, upper_bound=30, output_shape=c.shape, tolerance=tol
     )
-    np.testing.assert_allclose(x0, np.sqrt(c), atol=tol)
+
+    # The first two elements of c are negative, so there's no root.
+    # The corresponding results and errors should be NaN.
+    assert np.isnan(x0[0][0][0])
+    assert np.isnan(x0[0][0][1])
+
+    assert np.isnan(err[0][0][0])
+    assert np.isnan(err[0][0][1])
+
+    np.testing.assert_allclose(x0[0][0][2:], np.sqrt(c[0][0][2:]), atol=tol)
+    np.testing.assert_allclose(x0[0][1:], np.sqrt(c[0][1:]), atol=tol)
+    np.testing.assert_allclose(x0[1:], np.sqrt(c[1:]), atol=tol)
+
+
+def test_bisect_no_convergence():
+    def f(x):
+        return x**2 + 1  # No root
+
+    x0, err = bisect_v(
+        f,
+        lower_bound=-50,
+        upper_bound=50,
+        output_shape=(1,),
+    )
+    assert np.isnan(x0).all()
+    assert np.isnan(err).all()
 
 
 #