4b82c01c06bc63c2b07fa51f3fd5279f1b948872
JEREMY VERNAY <jeremy.vernay@rte-france.com>
2026-02-24 13:42:17 +0100

    feat(#78): get solar hour from local longitude
    Signed-off-by: JEREMY VERNAY <jeremy.vernay@rte-france.com>

--- src/thermohl/power/rte/solar_heating.py
+++ src/thermohl/power/rte/solar_heating.py
@@ -26,25 +26,16 @@ solar_radiation = _SRad(clean=CLEAN_AIR_COEFFICIENTS, indus=POLLUTED_AIR_COEFFIC
 
 
 def solar_irradiance(
-    latitude: floatArrayLike,
-    month: intArrayLike,
-    day: intArrayLike,
-    hour: floatArrayLike,
+    solar_altitude_rad: floatArrayLike,
 ) -> floatArrayLike:
     """Compute solar radiation.
-
     Difference with IEEE version are neither turbidity or altitude influence.
-
     Args:
-        latitude (float | numpy.ndarray): Latitude in radians.
-        month (int | numpy.ndarray): Month (1-12).
-        day (int | numpy.ndarray): Day of the month.
-        hour (float | numpy.ndarray): Hour of the day (0-24).
-
+        solar_altitude_rad (float | numpy.ndarray): solar altitude in radians.
     Returns:
         float | numpy.ndarray: Solar radiation value. Negative values are set to zero.
     """
-    solar_altitude_rad = sun.solar_altitude(latitude, month, day, hour)
+
     clearness_factor = solar_radiation.atmosphere_turbidity(
         np.rad2deg(solar_altitude_rad)
     )
@@ -55,6 +46,7 @@ class SolarHeating(SolarHeatingBase):
     def __init__(
         self,
         latitude: floatArrayLike,
+        longitude: floatArrayLike,
         azimuth: floatArrayLike,
         month: intArrayLike,
         day: intArrayLike,
@@ -73,17 +65,18 @@ class SolarHeating(SolarHeatingBase):
             azimuth (float | numpy.ndarray): Azimuth.
             month (int | numpy.ndarray): Month number (must be between 1 and 12).
             day (int | numpy.ndarray): Day of the month (must be between 1 and 28, 29, 30 or 31 depending on month).
-            hour (float | numpy.ndarray): Hour of the day (solar, must be between 0 and 23).
+            hour (float | numpy.ndarray): Hour of the day (must be between 0 and 24 excluded).
             outer_diameter (float | numpy.ndarray): external diameter.
             solar_absorptivity (numpy.ndarray): Solar absorption coefficient.
             measured_solar_irradiance (float | numpy.ndarray | None): Optional measured solar irradiance (W/m2).
         """
         self.solar_absorptivity = solar_absorptivity
+        solar_hour = sun.utc2solar_hour(hour, day, month, np.deg2rad(longitude))
+        solar_altitude_rad = sun.solar_altitude(
+            np.deg2rad(latitude), month, day, solar_hour
+        )
         if np.isnan(measured_solar_irradiance).all():
-            measured_solar_irradiance = solar_irradiance(
-                np.deg2rad(latitude), month, day, hour
-            )
-        solar_altitude_rad = sun.solar_altitude(np.deg2rad(latitude), month, day, hour)
+            measured_solar_irradiance = solar_irradiance(solar_altitude_rad)
         solar_azimuth_rad = sun.solar_azimuth(np.deg2rad(latitude), month, day, hour)
         incidence_angle_rad = np.arccos(
             np.cos(solar_altitude_rad) * np.cos(solar_azimuth_rad - np.deg2rad(azimuth))
