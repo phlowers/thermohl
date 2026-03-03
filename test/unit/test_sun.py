4cfabfe1c7fb555052a005bdf63256118ea93982
JEREMY VERNAY <jeremy.vernay@rte-france.com>
2026-03-03 10:31:44 +0100

    feat(#67): use datetime type for date and time parameters
    Signed-off-by: JEREMY VERNAY <jeremy.vernay@rte-france.com>

--- test/unit/test_sun.py
+++ test/unit/test_sun.py
@@ -5,29 +5,31 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
 
+from datetime import datetime
 import numpy as np
 from thermohl.sun import utc2solar_hour
 
 
 def test_scalar_input():
     # Test with scalar inputs
-    hour = 12 + 30 / 60 + 45 / 3600
-    day = 18
-    month = 5
+    # utc2solar_hour(datetime_utc, longitude)
+    dt = datetime(2025, 5, 18, 12, 30, 45)
     longitude = np.deg2rad(45)  # 45 degrees east
 
-    result = utc2solar_hour(hour, day, month, longitude)
+    result = utc2solar_hour(dt, longitude)
     expected_result = 15.57397066555581
     assert np.isclose(result, expected_result)
 
 
 def test_array_input():
     # Test with numpy array inputs
-    hours = np.array([12, 15, 18]) + np.array([30, 45, 0]) / 60
-    days = np.array([18, 2, 27])
-    months = np.array([3, 9, 12])
-    lons = np.deg2rad(np.array([0, 90, 135]))  # 45, 90, and 135 degrees east
+    dts = [
+        datetime(2025, 3, 18, 12, 30, 0),
+        datetime(2025, 9, 2, 15, 45, 0),
+        datetime(2025, 12, 27, 18, 0, 0),
+    ]
+    lons = np.deg2rad(np.array([0, 90, 135]))
 
-    result = utc2solar_hour(hours, days, months, lons)
+    result = utc2solar_hour(dts, lons)
     expected_result = np.array([12.35394, 21.76352, 26.976276])
     np.testing.assert_array_almost_equal(result, expected_result)
